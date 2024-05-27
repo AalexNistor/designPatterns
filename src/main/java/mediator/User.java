9214be1b0b0328756c9b498a896bb7f76062c152
Nistor Alex <nistor.alexandru229@gmail.com>
2024-05-27 21:01:16 +0300

    DesignPatternMediator

---
+++ src/main/java/mediator/User.java
@@ -0,0 +1,17 @@
+package mediator;
+
+import observer.Channel;
+
+public abstract class User {
+    protected String name;
+    protected ChatMediator chat;
+
+    public User(String name, ChatMediator chat) {
+        this.name = name;
+        this.chat = chat;
+    }
+
+    public abstract void sendMessage(String message);
+    public abstract void receiveMessage(String message);
+
+}
